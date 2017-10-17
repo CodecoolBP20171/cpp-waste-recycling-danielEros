1e8836691fa4c6a87bf368da73fec8db6e4ed5a8
Daniel Eros <drerosdaniel@gmail.com>
2017-10-17 13:44:28 +0200

    implement PaperGarbage class

---
+++ Garbages/PaperGarbage.cpp
@@ -0,0 +1,18 @@
+//
+// Created by eros on 17/10/17.
+//
+
+#include "PaperGarbage.h"
+
+PaperGarbage::PaperGarbage(string name) : Garbage(name) {
+    this->name = name;
+    isSqueezed = false;
+}
+
+void PaperGarbage::squeeze() {
+    this->isSqueezed = true;
+}
+
+bool PaperGarbage::getIsSqueezed() {
+    return isSqueezed;
+}
---
+++ Garbages/PaperGarbage.h
@@ -0,0 +1,22 @@
+//
+// Created by eros on 17/10/17.
+//
+
+#ifndef CPP_WASTE_RECYCLING_DANIELEROS_PAPERGARBAGE_H
+#define CPP_WASTE_RECYCLING_DANIELEROS_PAPERGARBAGE_H
+
+
+#include "Garbage.h"
+
+class PaperGarbage : public Garbage {
+public:
+    PaperGarbage(){ isSqueezed = false; }
+    PaperGarbage(string name);
+    void squeeze();
+    bool getIsSqueezed();
+private:
+    bool isSqueezed;
+};
+
+
+#endif //CPP_WASTE_RECYCLING_DANIELEROS_PAPERGARBAGE_H
